d2362c69d5b6f6ca2af34871943f2a07c1ba5fb6
Benjamin Sherman <benjamin@holyarmy.org>
2023-05-13 17:32:18 -0500

    refactor: improve script from PR suggestions

--- build/ublue-os-just/ublue-os-just.sh
+++ build/ublue-os-just/ublue-os-just.sh
@@ -1,10 +1,11 @@
-# Add uBlue's justfiles to user HOME if they don't already have a justfile.
+# Add uBlue's justfiles to users with home directories which lack a justfile.
 
-UBLUE_JUST=/usr/share/ublue-os/just
-USER_JUSTFILE="${HOME}/.justfile"
-if [ ! -f "${USER_JUSTFILE}" ]; then
+if [ ! -z "$HOME" ] && [ -d "$HOME" ] && [ ! -f "${HOME}/.justfile" ]; then
+  UBLUE_JUST=/usr/share/ublue-os/just
+  USER_JUSTFILE="${HOME}/.justfile"
+  touch "${USER_JUSTFILE}"
   if [ -f ${UBLUE_JUST}/main.just ]; then
-    cp ${UBLUE_JUST}/main.justfile "${USER_JUSTFILE}"
+    cat ${UBLUE_JUST}/main.just >> "${USER_JUSTFILE}"
   fi
   if [ -f ${UBLUE_JUST}/nvidia.just ] && [ rpm -q xorg-x11-drv-nvidia ]; then
     cat ${UBLUE_JUST}/nvidia.just >> "${USER_JUSTFILE}"
