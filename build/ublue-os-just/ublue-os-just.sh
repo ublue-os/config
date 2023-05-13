6e75367ccf3e77704aa1149037d14e8d82473d10
Benjamin Sherman <benjamin@holyarmy.org>
2023-05-13 16:08:55 -0500

    refactor: use standard path for just plus...
    This changes the ublue-os-just RPM by:
    - using /usr/share/ublue-os/just path (no double ublue-os)
    - renames "justfile" to "main.just"
    - adds "nvidial.just" and "custom.just"
    - improves profile script to smartly add appropriate just files
    
    Closes #41

--- build/ublue-os-just/ublue-os-just.sh
+++ build/ublue-os-just/ublue-os-just.sh
@@ -1,6 +1,16 @@
-# Add justfile to HOME if it's not already installed
+# Add uBlue's justfiles to user HOME if they don't already have a justfile.
 
-if [[ ! -f "${HOME}/.justfile" && -f /usr/share/ublue-os/ublue-os-just/justfile ]]; then
-  cp /usr/share/ublue-os/ublue-os-just/justfile "${HOME}/.justfile"
+UBLUE_JUST=/usr/share/ublue-os/just
+USER_JUSTFILE="${HOME}/.justfile"
+if [ ! -f "${USER_JUSTFILE}" ]; then
+  if [ -f ${UBLUE_JUST}/main.just ]; then
+    cp ${UBLUE_JUST}/main.justfile "${USER_JUSTFILE}"
+  fi
+  if [ -f ${UBLUE_JUST}/nvidia.just ] && [ rpm -q xorg-x11-drv-nvidia ]; then
+    cat ${UBLUE_JUST}/nvidia.just >> "${USER_JUSTFILE}"
+  fi
+  if [ -f ${UBLUE_JUST}/custom.just ]; then
+    cat ${UBLUE_JUST}/custom.just >> "${USER_JUSTFILE}"
+  fi
 fi
 
