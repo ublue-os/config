0c42ec37edcf7a10cfc47abdf3bb4211a358b448
Eino Rauhala <60004820+EinoHR@users.noreply.github.com>
2023-05-22 12:51:13 +0000

    chore: make `just` unstable by default (#49)
    * This change allows us to recommend using just includes by default

--- build/ublue-os-just/ublue-os-just.sh
+++ build/ublue-os-just/ublue-os-just.sh
@@ -1,17 +1,2 @@
-# Add uBlue's justfiles to users with home directories which lack a justfile.
-
-if [ ! -z "$HOME" ] && [ -d "$HOME" ] && [ ! -f "${HOME}/.justfile" ]; then
-  UBLUE_JUST=/usr/share/ublue-os/just
-  USER_JUSTFILE="${HOME}/.justfile"
-  touch "${USER_JUSTFILE}"
-  if [ -f ${UBLUE_JUST}/main.just ]; then
-    cat ${UBLUE_JUST}/main.just >> "${USER_JUSTFILE}"
-  fi
-  if [ -f ${UBLUE_JUST}/nvidia.just ] && [ rpm -q ublue-os-nvidia-addons ]; then
-    cat ${UBLUE_JUST}/nvidia.just >> "${USER_JUSTFILE}"
-  fi
-  if [ -f ${UBLUE_JUST}/custom.just ]; then
-    cat ${UBLUE_JUST}/custom.just >> "${USER_JUSTFILE}"
-  fi
-fi
+alias just="just --unstable"
 
