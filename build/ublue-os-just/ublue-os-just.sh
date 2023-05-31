45b06aea7fb3c783b790a7b597c1b692dcceac5d
Benjamin Sherman <benjamin@holyarmy.org>
2023-05-31 11:52:06 -0500

    feat: create default justfile for users
    When a user does not have a $HOME/.justfile, provide a default which
    uses `just includes` from our provided files as documented on the
    website.
    
    https://ublue.it/guide/just/

--- build/ublue-os-just/ublue-os-just.sh
+++ build/ublue-os-just/ublue-os-just.sh
@@ -1,2 +1,11 @@
 alias just="just --unstable"
 
+# Add uBlue's justfiles to users with home directories which lack a justfile.
+
+if [ ! -z "$HOME" ] && [ -d "$HOME" ] && [ ! -f "${HOME}/.justfile" ]; then
+  cat > "${HOME}/.justfile" << EOF
+!include /usr/share/ublue-os/just/main.just
+!include /usr/share/ublue-os/just/nvidia.just
+!include /usr/share/ublue-os/just/custom.just
+EOF
+fi
