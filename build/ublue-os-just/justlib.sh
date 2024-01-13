201bade7adfc88475b4dd39e22d735a5e8e42b92
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2024-01-13 21:55:14 +0100

    feat(just): add confirm function

--- build/ublue-os-just/justlib.sh
+++ build/ublue-os-just/justlib.sh
@@ -23,7 +23,6 @@ declare -r hidden=$'\033[8m'
 ### Remove Text Formating
 ########
 declare -r normal=$'\033[0m'
-declare -r reset="$normal"
 declare -r unbold=$'\033[21m'
 declare -r undim=$'\033[22m'
 declare -r nounderline=$'\033[24m'
@@ -96,10 +95,21 @@ function bg (){
 ## Useful functions we use a lot
 ########
 ## Function to generate a choice selection and return the selected choice
-# CHOICE=$(Ui_choice option1 option2 "option 3")
+# CHOICE=$(Choice option1 option2 "option 3")
 # *user selects "option 3"*
 # echo "$CHOICE" will return "option 3"
 function Choose (){
     CHOICE=$(ugum choose "$@")
     echo "$CHOICE"
+}
+
+## Function to generate a confirm dialog and return the selected choice
+# CHOICE=$(Confirm "Are you sure you want to do this?")
+# *user selects "No"*
+# echo "$CHOICE" will return "1"
+# 0 = Yes
+# 1 = No
+function Confirm (){
+    PROMPT=$(ugum confirm "$@")
+    echo $?
 }
