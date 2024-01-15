a89532b461252aebe8c55861b618519dcf14bf24
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2024-01-15 16:17:39 +0100

    feat(just): add support for distrobox assemble

--- build/ublue-os-just/libfunctions.sh
+++ build/ublue-os-just/libfunctions.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
+# shellcheck disable=SC2154
 ########
-## Useful functions we use a lot
+## Useful functions we use a lot, if you want to use them, source libjust.sh
+## Dependencies libformatting.sh and libcolors.sh
 ########
 ## Function to generate a choice selection and return the selected choice
 # CHOICE=$(Choice option1 option2 "option 3")
@@ -45,13 +47,69 @@ function Distrobox (){
 
     # If a custom home directory is not specified
     if [ -z "$HOMEDIR" ]; then
-        distrobox-create --nvidia --image "$IMAGE" -n "$NAME" "${@:2}" -Y
+        distrobox create --nvidia -Y --image "$IMAGE" -n "$NAME" "${@:2}"
     else
         # Make the custom homedir path if it does not exist
         if [ ! -d "$HOMEDIR" ]; then
             mkdir -p "$HOMEDIR"
         fi
         # Create distrobox with custom home path
-        distrobox-create --nvidia --image "$IMAGE" -n "$NAME" -H "$HOMEDIR" "${@:3}" -Y
+        distrobox create --nvidia -Y --image "$IMAGE" -n "$NAME" -H "$HOMEDIR" "${@:3}"
     fi
+}
+
+########
+## Function to assemble pre-defined distrobox containers from manifest files
+########
+# 
+function Assemble(){
+    # Set defaults
+    ACTION="create"
+    FILE="/etc/distrobox/distrobox.ini"
+    NAME=""
+
+    # If an action is provided
+    if [ -n "$1" ]; then
+        # Set ACTION to the action specified
+        # and remove "noconfirm" from $1 when assigning it to ACTION
+        ACTION="${1/noconfirm/}"
+    fi
+
+    # If a filename is provided
+    if [ -n "$2" ]; then
+        # Set FILE to the provided filename
+        FILE="$2"
+    fi
+
+    # If a container name is provided
+    if [ -n "$3" ]; then
+        # Set distrobox name to provided name
+        NAME="$3"
+    else
+        if [[ ! "$1" =~ ^noconfirm ]]; then
+            # Ask user if they REALLY want to assemble all the containers
+            echo -e "${b}WARNING${n}: This will assemble and ${u}replace${n}\nALL containers defined in ${b}$FILE${n}."
+            CONFIRM=$(Confirm "Are you sure you want to do this?")
+            if [ "$CONFIRM" == "1" ]; then
+                echo "Aborting..."
+                return 1
+            fi
+        fi
+        # Run the distrobox assemble command
+        distrobox assemble "$ACTION" --file "$FILE" --replace --dry-run
+    fi
+    
+    # If we do not want confirmations
+    if [[ ! "$1" =~ ^noconfirm ]]; then
+        # Ask the user if they really want to replace $NAME container
+        echo -e "${b}WARNING${n}: This will assemble and ${u}replace${n} the container ${b}$NAME${n}\nwith the one defined in ${b}$FILE${n}."
+        CONFIRM=$(Confirm "Are you sure you want to do this?")
+        if [ "$CONFIRM" == "1" ]; then
+            echo "Aborting..."
+            return 1
+        fi
+    fi
+
+    # Run the distrobox assemble command
+    distrobox assemble "$ACTION" --file "$FILE" --name "$NAME" --replace --dry-run
 }
