0ced9c4d8349bd4bb5dace3e6ea420df18a1f691
Benjamin Sherman <benjamin@holyarmy.org>
2024-01-29 12:44:46 -0600

    feat: add more flexibility to user-motd
    Changes are:
    1. allow /usr/libexec/ublue-motd as executable, dynamic motd
    2. change to /etc/user-motd for static motd, allowing choice to
       customizate user-motd at runtime if pre-generated is not desired

--- build/ublue-os-just/etc-profile.d/user-motd.sh
+++ build/ublue-os-just/etc-profile.d/user-motd.sh
@@ -1,7 +1,9 @@
 if test -d "$HOME"; then
   if test ! -e "$HOME"/.config/no-show-user-motd; then
-    if test -s "/usr/share/ublue-os/user-motd"; then
-      cat /usr/share/ublue-os/user-motd
+    if test -x "/usr/libexec/ublue-motd"; then
+      /usr/libexec/ublue-motd
+    elif test -s "/etc/user-motd"; then
+      cat /etc/user-motd
     fi
   fi
 fi
