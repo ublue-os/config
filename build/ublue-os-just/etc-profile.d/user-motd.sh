446a8f7f6d7e857eb3638f7751f3a1e4b1b50c10
Drew Moseley <drew@moseleynet.net>
2024-07-14 13:46:09 -0400

    feat: Prevent double sourcing of user-motd (#289)
    Signed-off-by: Drew Moseley <drew@moseleynet.net>

--- build/ublue-os-just/etc-profile.d/user-motd.sh
+++ build/ublue-os-just/etc-profile.d/user-motd.sh
@@ -1,9 +1,13 @@
-if test -d "$HOME"; then
-  if test ! -e "$HOME"/.config/no-show-user-motd; then
-    if test -x "/usr/libexec/ublue-motd"; then
-      /usr/libexec/ublue-motd
-    elif test -s "/etc/user-motd"; then
-      cat /etc/user-motd
+# Prevent doublesourcing
+if [ -z "$USERMOTDSOURCED" ]; then
+  USERMOTDSOURCED="Y"
+  if test -d "$HOME"; then
+    if test ! -e "$HOME"/.config/no-show-user-motd; then
+      if test -x "/usr/libexec/ublue-motd"; then
+        /usr/libexec/ublue-motd
+      elif test -s "/etc/user-motd"; then
+        cat /etc/user-motd
+      fi
     fi
   fi
 fi
