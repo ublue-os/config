705c0dffb9a2179d1f6563caaa4fd086d413b01c
Kristiyan Georgiev <2003kristiyan@gmail.com>
2025-01-16 10:29:50 +0100

    fix: separate just and ujust

--- build/ublue-os-just/etc-profile.d/ublue-os-just.sh
+++ build/ublue-os-just/etc-profile.d/ublue-os-just.sh
@@ -1,19 +1,17 @@
-# Add uBlue's justfiles to users with home directories which lack a justfile.
+# Add a custom justfile to users with home directories which lack a justfile.
 
 if [ ! -z "$HOME" ] && [ -d "$HOME" ] && [ ! -f "${HOME}/.justfile" ]; then
   cat > "${HOME}/.justfile" << EOF
-import "/usr/share/ublue-os/justfile"
+# You can add your own commands here!
+# These commands are separate from the ujust commands.
+# Learn more about just at https://github.com/casey/just
+
+# Choose
+_default:
+  just --choose
+
+# Edit the justfile
+edit: 
+  just -e
 EOF
-fi
-
-if [ -f "${HOME}/.justfile" ]; then
-  if ! grep -Fxq 'import "/usr/share/ublue-os/justfile"' "${HOME}/.justfile"; then
-    # Remove any lines we may have added previously.
-    sed -i '/!include \/usr\/share\/ublue-os\/just\/.*.just/d' "${HOME}/.justfile"
-    sed -i '/!include \/usr\/share\/ublue-os\/justfile/d' "${HOME}/.justfile"
-
-    # Point to the new main justfile, place it as the first line
-    echo '# You can add your own commands here! For documentation, see: https://ublue.it/guide/just/' | tee -a "${HOME}/.justfile"
-    echo 'import "/usr/share/ublue-os/justfile"' | tee -a "${HOME}/.justfile"
-  fi
 fi
